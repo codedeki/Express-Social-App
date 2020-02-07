5deb4edafd65c4824134f5125c0f44bf08a022c7
codedeki <serbian_5@hotmail.com>
2020-02-07 00:08:02 -0500

    Following Users Feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,31 @@
 const User = require("../models/User");
 const Post = require("../models/Post");
+const Follow = require("../models/Follow");
+
+//reusable function for multiple routes
+exports.sharedProfileData = async function(req, res, next) {
+    let isVisitorsProfile = false;
+    let isFollowing = false;
+    if (req.session.user) {
+        isVisitorsProfile = req.profileUser._id.equals(req.session.user._id) //check user profile id against current session user id
+       isFollowing = await Follow.isVisitorFollowing(req.profileUser._id, req.visitorId) //check to see if visitor is following user
+    } 
+
+    req.isVisitorsProfile = isVisitorsProfile; //makes the true or false value available for the next function in route
+    req.isFollowing = isFollowing;
+    //retrieve post, follower, and following counts
+    let postCountPromise = Post.countPostsByAuthor(req.profileUser._id)
+    let followerCountPromise = Follow.countFollowersById(req.profileUser._id)
+    let followingCountPromise = Follow.countFollowingById(req.profileUser._id)
+    let [postCount, followerCount, followingCount] = await Promise.all([postCountPromise, followerCountPromise, followingCountPromise]) //faster performance, store in variables and get all promises regardless of order before moving on to next
+    
+    req.postCount = postCount; //set req. values to our custom variables for easy access 
+    req.followerCount = followerCount;
+    req.followingCount = followingCount; 
+
+    next()
+
+}
 
 exports.mustBeLoggedIn = function(req, res, next) {
     if (req.session.user) {
@@ -13,7 +39,7 @@ exports.mustBeLoggedIn = function(req, res, next) {
 }
 
 exports.login = function(req, res) {
-    let user = new User(req.body); //pass in form data req.body
+    let user = new User(req.body); //pass in from data req.body
     user.login().then(function(result) {
         //make each user session data unique & persistent
         req.session.user = {avatar: user.avatar, username: user.data.username, _id: user.data._id}
@@ -51,9 +77,11 @@ exports.register = function(req, res) {
     }); 
 }
 
-exports.home = function(req, res) {
+exports.home = async function(req, res) {
     if (req.session.user) {
-        res.render('home-dashboard');
+        //fetch feed of posts for current user
+        let posts = await Post.getFeed(req.session.user._id); //render list of posts to home page
+        res.render('home-dashboard', {posts: posts});
     } else {
         res.render('home-guest', {regErrors: req.flash('regErrors')}); //show flash message if error
     }
@@ -72,11 +100,51 @@ exports.profilePostsScreen = function(req, res) {
     //view posts by author id
     Post.findByAuthorId(req.profileUser._id).then((posts) => {
         res.render('profile', {
+            currentPage: "posts",
             posts: posts, 
             profileUsername: req.profileUser.username,
-            profileAvatar: req.profileUser.avatar
+            profileAvatar: req.profileUser.avatar,
+            isFollowing: req.isFollowing,
+            isVisitorsProfile: req.isVisitorsProfile,
+            counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount}
         });
     }).catch(() => {
         res.render("404")
     })
+}
+
+exports.profileFollowersScreen = async function(req, res) {
+   try { 
+       //populate followers page by users and avatars and pass into template
+    let followers = await Follow.getFollowersById(req.profileUser._id)
+    res.render('profile-followers', { 
+        currentPage: "followers",
+        followers: followers,
+        profileUsername: req.profileUser.username,
+        profileAvatar: req.profileUser.avatar,
+        isFollowing: req.isFollowing,
+        isVisitorsProfile: req.isVisitorsProfile,
+        counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount}
+    })
+   } catch {
+    res.render("404");
+   }
+}
+
+exports.profileFollowingScreen = async function(req, res) {
+   try { 
+       //populate followers page by users and avatars and pass into template
+    let following = await Follow.getFollowingById(req.profileUser._id)
+    res.render('profile-following', { 
+        currentPage: "following",
+        following: following,
+        profileUsername: req.profileUser.username,
+        profileAvatar: req.profileUser.avatar,
+        isFollowing: req.isFollowing,
+        isVisitorsProfile: req.isVisitorsProfile,
+        counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount}
+    })
+   } catch {
+    res.render("404");
+   }
 }
---
+++ models/Follow.js
@@ -0,0 +1,134 @@
+const usersCollection = require('../db').db().collection("users");
+const followsCollection = require('../db').db().collection("follows"); //create follows collection in db
+const ObjectID = require('mongodb').ObjectID;
+const User = require('./User');
+
+let Follow = function(followedUsername, authorId) {
+    this.followedUsername = followedUsername;
+    this.authorId = authorId;
+    this.errors = [];
+}
+
+
+Follow.prototype.cleanUp = async function() {
+    if (typeof(this.followedUsername) != "string") {this.followedUsername = ""}
+}
+
+Follow.prototype.validate = async function(action) {
+    //followedUsername must exist in database
+    let followedAccount = await usersCollection.findOne({username: this.followedUsername})
+     if (followedAccount) {
+        this.followedId = followedAccount._id
+     } else {
+        this.errors.push("You cannot follow a user that does not exist.")
+     }
+     
+     let doesFollowAlreadyExist = await followsCollection.findOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)})
+     if (action == "create") {
+         if (doesFollowAlreadyExist) {this.errors.push("You are already following this user.")}
+     }
+     if (action == "delete") {
+         if (!doesFollowAlreadyExist) {this.errors.push("You cannot stop following someone you do not already follow.")}
+     }
+
+     // cannot follow yourself
+        if (this.followedId.equals(this.authorId)) {this.errors.push("You cannot follow yourself.")}
+
+}
+
+Follow.prototype.create = function() {
+    return new Promise(async (resolve, reject) => {
+        this.cleanUp()
+        await this.validate("create")
+        if (!this.errors.length) {
+            await followsCollection.insertOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)})
+            resolve()
+        } else {
+            reject(this.errors)
+        }
+    })
+}
+
+Follow.prototype.delete = function() {
+    return new Promise(async (resolve, reject) => {
+        this.cleanUp()
+        await this.validate("delete")
+        if (!this.errors.length) {
+            await followsCollection.deleteOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)})
+            resolve()
+        } else {
+            reject(this.errors)
+        }
+    })
+}
+
+Follow.isVisitorFollowing = async function(followedId, visitorId) {
+    let followDoc = await followsCollection.findOne({followedId: followedId, authorId: new ObjectID(visitorId)})
+    if (followDoc) {
+        return true
+    } else {
+        return false
+    }
+}
+
+Follow.getFollowersById = function(id) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let followers = await followsCollection.aggregate([ //aggregate is native to mongodb
+                {$match: {followedId: id}},
+                {$lookup: {from: "users", localField: "authorId", foreignField: "_id", as: "userDoc"}},
+                {$project: {
+                    username: {$arrayElemAt: ["$userDoc.username", 0]}, //in first item in array grabbing username and email (to get gravitar photo) properties in databse
+                    email: {$arrayElemAt: ["$userDoc.email", 0]}
+                }} 
+            ]).toArray() //toArray is native to JavaScript; convert to array so we can map data
+            followers = followers.map((follower) => {
+                //create a user
+                let user = new User(follower, true) //figures out a grativar based on user's email address
+                return {username: follower.username, avatar: user.avatar}
+            })
+            resolve(followers)
+        } catch {
+            reject()
+        }
+    })
+}
+
+Follow.getFollowingById = function(id) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let following = await followsCollection.aggregate([
+                {$match: {authorId: id}},
+                {$lookup: {from: "users", localField: "followedId", foreignField: "_id", as: "userDoc"}}, //show list of users the person is following
+                {$project: {
+                    username: {$arrayElemAt: ["$userDoc.username", 0]}, //in first item in array grabbing username and email (to get gravitar photo) properties in databse
+                    email: {$arrayElemAt: ["$userDoc.email", 0]}
+                }} 
+            ]).toArray() //aggregate and to array native to mongodb
+            following = following.map((followedUser) => {
+                //create a user
+                let user = new User(followedUser, true) //figures out a grativar based on user's email address
+                return {username: follower.username, avatar: user.avatar}
+            })
+            resolve(following)
+        } catch {
+            reject()
+        }
+    })
+}
+
+Follow.countFollowersById = function(id) {
+    return new Promise(async (resolve, reject) => {
+        let followerCount = await followsCollection.countDocuments({followedId: id})
+        resolve(followerCount)
+    })
+}
+
+Follow.countFollowingById = function(id) {
+    return new Promise(async (resolve, reject) => {
+        let count = await followsCollection.countDocuments({authorId: id})
+        resolve(count) 
+    })
+}
+
+module.exports = Follow;
